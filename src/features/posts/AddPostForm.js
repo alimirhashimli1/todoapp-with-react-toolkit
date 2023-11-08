d4d6f9c6a45aac935734bf1c784ae55952bcd0e7
alimirhashimli <alimirhashimli@gmail.com>
2023-11-08 02:46:56 +0100

    first commit

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { postsAdded } from "./postsSlice.js";
+import { selectAllUsers } from "../users/usersSlice.js";
+
+const AddPostForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
+
+  const onAuthorChanged = (e) => setUserId(e.target.value);
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postsAdded(title, content, userId));
+      setTitle("");
+      setContent("");
+    }
+  };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="">Author:</label>
+        <select
+          id="post Author"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        >
+          <option value=""></option>
+          {usersOptions}
+        </select>
+
+        <label htmlFor="postContent">Post Content:</label>
+        <input
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,36 @@
+import {createSlice, nanoid} from "@reduxjs/toolkit"
+import {sub} from "date-fns"
+
+
+const initialState = [
+    {id: 1, title: "Learning Redux Toolkit", content: "I've heard ood things", date: sub(new Date(), {minutes: 10}).toISOString()},
+    {id: 2, title: "Slicez...", content: "The mopre I slice, the more I want pizza", date: sub(new Date(), {minutes: 5}).toISOString()},
+
+]
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        postsAdded: {
+            reducer(state, action){
+            state.push(action.payload)
+        },
+            prepare (title, content, userId){
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        date: new Date().toISOString(),
+                        userId
+                    }
+                }
+            }
+    }
+}
+})
+
+export const selectAllPosts = state => state.posts;
+export const {postsAdded  } = postsSlice.actions;
+ export default postsSlice.reducer
